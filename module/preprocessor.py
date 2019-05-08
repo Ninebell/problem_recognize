eb56a2c99510a71e39ce46918fa54020932b8795
Ninebell <ninebell94@gmail.com>
2019-05-08 17:18:16 +0900

    add function which split the horizontal regions

--- module/preprocessor.py
+++ module/preprocessor.py
@@ -26,6 +26,7 @@ def make_rect(points):
 def draw_roi(image, roi, color, thin):
     return cv2.rectangle(image, roi[0], roi[1], color, thin)
 
+
 def points_to_rectangles(contours):
     rectangles = []
     for points in contours:
@@ -43,8 +44,9 @@ def img_to_binary(image):
 def size_filter(region, arg):
     return region.area > arg[0]
 
+
 # find object roi
-def make_regions(image, filter, filter_arg, reverse=True):
+def make_regions(image, filters, filter_args, reverse=True):
     image = img_to_binary(image)
     if reverse:
         image = 255-image
@@ -53,8 +55,14 @@ def make_regions(image, filter, filter_arg, reverse=True):
     basic_roi = points_to_rectangles(contours)
     final_roi = []
     for roi in basic_roi:
-        if size_filter(roi, filter_arg):
+        filter_ok = True
+        for idx, roi_filter in enumerate(filters):
+            filter_ok = roi_filter(roi, filter_args[idx])
+            if not filter_ok:
+                break
+        if filter_ok:
             final_roi.append(roi)
+    final_roi.sort(key=lambda roi: roi.area)
     return final_roi
 
 
@@ -63,18 +71,43 @@ def union_intersection(img, regions):
 
     for region in regions:
         bin_img = draw_roi(bin_img, region.points, 0, -1)
+        bin_img = draw_roi(bin_img, region.points, 0, 2)
 
-    regions = make_regions(input_img, size_filter, [10,])
+    regions = make_regions(bin_img, [size_filter], [[10, ]])
+    regions.sort(key=lambda roi: roi.area)
     return regions
+
+
+def split_horizontal_regions(img, regions):
+    bin_img = img_to_binary(img)
+    height, width = bin_img.shape
+    roi_img = bin_img.copy()
+    for region in regions:
+        new_points = [(0, region.points[0][1]), (width, region.points[1][1])]
+        roi_img = draw_roi(bin_img, new_points, 0, -1)
+
+    cv_imshow("roi", roi_img, 0)
+
+
+def erase_region(img, regions):
+    color = 255
+    if len(img.shape) == 3:
+        color = [255, 255, 255]
+
+    erase_regions = regions[-3:]
+
+    for region in erase_regions:
+        img = draw_roi(img, region.points, color, -1)
+    return img, regions[:-3]
 
 
 if __name__ == "__main__":
     input_img = sample_img()
     roi_img = input_img.copy()
-    rois = make_regions(input_img, size_filter, [10,])
+    rois = make_regions(input_img, [size_filter], [[10, ]])
 
     for roi in rois:
-        roi_img = draw_roi(roi_img, roi.points, [0,255,0], 1)
+        roi_img = draw_roi(roi_img, roi.points, [0, 255, 0], 1)
 
     cv_imshow("roi", roi_img, 0)
 
@@ -87,3 +120,8 @@ if __name__ == "__main__":
         roi_img = draw_roi(roi_img, roi.points, [0, 255, 0], -1)
 
     cv_imshow("roi", roi_img, 0)
+
+    erased_img, rois = erase_region(input_img, rois)
+
+    cv_imshow("erase", erased_img, 0)
+    split_horizontal_regions(erased_img, rois)
