f87623dfd91898ebf23a7713b3f854a8acf2e7ce
Ninebell <ninebell94@gmail.com>
2019-05-16 14:28:45 +0900

    add some util function

--- module/util.py
+++ module/util.py
@@ -1,4 +1,9 @@
 import cv2
+import numpy as np
+
+
+def cv_imload(path):
+    return cv2.imread(path, cv2.IMREAD_COLOR)
 
 
 def cv_imshow(window_name, image, time):
@@ -8,4 +13,10 @@ def cv_imshow(window_name, image, time):
 
 
 def sample_img():
-    return cv2.imread("../test0.PNG", cv2.IMREAD_COLOR)
+    return cv2.imread("./test0.PNG", cv2.IMREAD_COLOR)
+
+
+def mid_values(value_list, method):
+    mid = len(value_list)
+    value_list.sort(key=lambda th: method(th))
+    return method(value_list[mid//2])
