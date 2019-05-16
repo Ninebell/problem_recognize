5282012d8532bf11384212f641c5c8eb9f5ea21e
Ninebell <ninebell94@gmail.com>
2019-05-16 14:44:52 +0900

    use --path for setting image path

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from module.util import *
+
 from module.preprocessor import *
 from model.Hangul import *
+from module.parser import *
+
+
 if __name__ == "__main__":
+    img_path = img_path()
+    #img = cv_imload(img_path)
     img = sample_img()
+    cv_imshow("input", img, 0)
     words = split_img_to_words(img)
 
     model = load_model("model/model1.h5")
